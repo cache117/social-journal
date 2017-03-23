4ba375fb975c8cde5863f1a0dc04504081563839
Cache Staheli <cache@byu.edu>
2017-03-22 21:36:55 -0600

    Stub out menu Intents

--- SocialJournal/app/src/main/java/edu/byu/cs456/journall/social_journal/MainActivity.java
+++ SocialJournal/app/src/main/java/edu/byu/cs456/journall/social_journal/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.byu.cs456.journall.social_journal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,18 +84,48 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
+            navigateHome();
             // send to home
         } else if (id == R.id.nav_add_note) {
+            addNote();
             //send to note page
         } else if (id == R.id.nav_add_picture) {
+            addPicture();
             //send to picture page
         } else if (id == R.id.nav_show_calendar) {
+            showCalendar();
             //send to calendar page
         } else if (id == R.id.nav_settings) {
             //send to settings page
+            openSettings();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void openSettings() {
+        //Intent settings = new Intent(this, Settings.class);
+//        startActivity(settings);
+    }
+
+    private void showCalendar() {
+        Intent calendar = new Intent(this, Calendar.class);
+        startActivity(calendar);
+    }
+
+    private void addPicture() {
+        Intent pictures = new Intent(this, AddPicture.class);
+        startActivity(pictures);
+    }
+
+    private void addNote() {
+        Intent newNote = new Intent(this, AddNote.class);
+        startActivity(newNote);
+    }
+
+    private void navigateHome() {
+        Intent home = new Intent(this, MainActivity.class);
+        startActivity(home);
     }
 }
