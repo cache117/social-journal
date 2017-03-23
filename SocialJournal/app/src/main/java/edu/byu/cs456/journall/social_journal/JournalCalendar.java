04ef45ca29fe3d8a8289aa13efffae58b26f9073
Cache Staheli <cache@byu.edu>
2017-03-23 09:43:13 -0600

    Add calendar

---
+++ SocialJournal/app/src/main/java/edu/byu/cs456/journall/social_journal/JournalCalendar.java
@@ -0,0 +1,30 @@
+package edu.byu.cs456.journall.social_journal;
+
+import android.icu.util.Calendar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class JournalCalendar extends AppCompatActivity {
+    private CalendarView calendar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        initializeCalendar();
+    }
+
+    private void initializeCalendar() {
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day)
+            {
+                Toast.makeText(getApplicationContext(), day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
