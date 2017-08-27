04f56a80316313a23a0d8cfc793f85210a6ebea5
Cache Staheli <cache@byu.edu>
2017-08-27 17:24:53 -0600

    Add Callback URI function

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -8,13 +8,17 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 exports.bigben = functions.https.onRequest((req, res) => {
-	  const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
-	  res.status(200).send(`<!doctype html>
-		      <head>
-		        <title>Time</title>
-		      </head>
-		      <body>
-		        ${'BONG '.repeat(hours)}
-		      </body>
-		    </html>`);
-	});
+    const hours = (new Date().getHours() % 12) + 1 // london is UTC + 1hr;
+    res.status(200).send(`<!doctype html>
+		  <head>
+			<title>Time</title>
+		  </head>
+		  <body>
+			${'BONG '.repeat(hours)}
+		  </body>
+		</html>`);
+});
+
+exports.redirect = functions.https.onRequest((req, res) => {
+	res.status(301).redirect('socialjournal://redirect');
+});
